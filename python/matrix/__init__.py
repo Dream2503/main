e5b4fccaf0ca6b23e4cf03e16e6cca66b2ab586f
Dream2503 <swapnarajmohanty2503.com>
2024-12-28 09:11:15 +0530

    commit

---
+++ python/matrix/__init__.py
@@ -0,0 +1,3 @@
+from .matrix import Matrix
+from .equation import Variable, Polynomial, PolynomialError
+from .number import Number
--- python/test.py
+++ python/test.py
@@ -1,4 +1,9 @@
 from matrix import Matrix
 
-m = Matrix(4, 5, matrix=[[10, 2, 2, 0, 3], [0, 10, 4, 6, 0], [0, 0, 10, 6, 0], [0, 0, 0, 10, 25]])
+m = Matrix(4, 5, matrix=[
+    [10, 2, 2, 0, 3],
+    [0, 10, 4, 6, 0],
+    [0, 0, 10, 6, 0],
+    [0, 0, 0, 10, 25]
+])
 print(m.gauss_elimination())
