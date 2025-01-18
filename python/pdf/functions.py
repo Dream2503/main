62baa8fe5f15d3827e2a0fba96a8b24b04d3b135
Dream2503 <swapnarajmohanty2503.com>
2025-01-19 00:09:31 +0530

    commit

--- python/pdf/functions.py
+++ python/pdf/functions.py
@@ -1,8 +1,8 @@
-from os import makedirs, remove
+from shutil import rmtree
 from PyPDF2 import PdfMerger, PdfReader, PdfWriter, PageObject
-from pdfminer.high_level import extract_text
-from pymupdf import Document
-import fitz
+from pymupdf import Document, Page
+import fitz, os
+
 
 def merge(path_list: list[str], output: str) -> None:
     pdf_merger: PdfMerger = PdfMerger()
@@ -10,7 +10,7 @@ def merge(path_list: list[str], output: str) -> None:
     for pdf in path_list:
         pdf_merger.append(pdf)
 
-    with open(f"./output/{output}", 'wb') as f:
+    with open(f"./output/{output}.pdf", 'wb') as f:
         pdf_merger.write(f)
 
 
@@ -23,29 +23,88 @@ def split(path: str, split_range: tuple[tuple[int, int]], output_prefix: str) ->
         for page_num in range(start - 1, end):
             pdf_writer.add_page(pdf_reader.pages[page_num])
 
-        output_filename: str = f"{output_prefix}_split_{start}-{end}.pdf"
-
-        with open(f"./output/{output_filename}", 'wb') as output_pdf:
+        with open(f"./output/{output_prefix}_split_{start}-{end}.pdf", 'wb') as output_pdf:
             pdf_writer.write(output_pdf)
 
 
-def pdf_to_text(path: str, output: str) -> None:
-    with open(f"./output/{output}", 'w', encoding='utf-8') as f:
+def to_text(path: str, output: str) -> None:
+    from pdfminer.high_level import extract_text
+
+    with open(f"./output/{output}.txt", 'w', encoding='utf-8') as f:
         f.write(extract_text(path))
 
 
-def images_from_pdf(pdf_path: str, output_dir: str) -> None:
+def extract_image(pdf_path: str, output_dir: str) -> None:
     pdf_document: Document = fitz.open(pdf_path)
 
     for page_num in range(len(pdf_document)):
-        page = pdf_document[page_num]
+        page: Page = pdf_document[page_num]
         images = page.get_images(full=True)
 
         for img_index, img in enumerate(images):
             image = pdf_document.extract_image(img[0])
             image_bytes = image["image"]
             image_path: str = f"./output/{output_dir}/page_{page_num + 1}_img_{img_index + 1}.{image["ext"]}"
-            makedirs(f"./output/{output_dir}", exist_ok=True)
+            os.makedirs(f"./output/{output_dir}", exist_ok=True)
 
             with open(image_path, "wb") as img_file:
                 img_file.write(image_bytes)
+
+
+def rotate(path: str, page_nos: tuple[int] | None, rotation_angle: int, output: str):
+    reader: PdfReader = PdfReader(path)
+    writer: PdfWriter = PdfWriter()
+    page_nums: set[int] | None = set(page_nos) if page_nos else None
+
+    for page_num in range(len(reader.pages)):
+        page: PageObject = reader.pages[page_num]
+
+        if page_nums is None or (page_nums is not None and (page_num + 1) in page_nums):
+            page.rotate(rotation_angle)
+
+        writer.add_page(page)
+
+    with open(f"./output/{output}.pdf", "wb") as output_file:
+        writer.write(output_file)
+
+
+def delete(path: str, page_nos: tuple[int], output: str):
+    reader: PdfReader = PdfReader(path)
+    writer: PdfWriter = PdfWriter()
+    page_nums: set[int] = set(page_nos)
+
+    for page_num in range(len(reader.pages)):
+        if page_num + 1 not in page_nums:
+            writer.add_page(reader.pages[page_num])
+
+    with open(f"./output/{output}.pdf", "wb") as output_file:
+        writer.write(output_file)
+
+
+def add_watermark(path: str, watermark: str, output: str):
+    pdf_document: Document = fitz.open(path)
+    watermark_color: tuple[int, int, int] = (0.5, 0.5, 0.5)
+    font_size: int = 50
+
+    for page in pdf_document:
+        width, height = page.rect.width, page.rect.height
+        page.insert_text(
+            (width / 2, height / 2),
+            watermark,
+            fontsize=font_size,
+            rotate=45,
+            color=watermark_color,
+        )
+
+    pdf_document.save(f"./output/{output}.pdf")
+
+
+def clear_directory(directory_path):
+    for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
+
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.remove(file_path)
+
+        elif os.path.isdir(file_path):
+            rmtree(file_path)
--- python/pdf/main.py
+++ python/pdf/main.py
@@ -1,5 +1,13 @@
-from functions import *
-from os import listdir
+try:
+    from functions import *
+
+except ModuleNotFoundError:
+    from subprocess import  run
+    run(f'start cmd /k "pip install PyPDF2 pdfminer.six pymupdf && exit"', shell=True)
+    del run
+
+finally:
+    from functions import *
 
 """
 Basic Features:
@@ -59,7 +67,7 @@ Accessibility Checks: Validate PDFs for accessibility compliance (e.g., WCAG).
 
 MAIN_MENU_1 = """
 Note: place all your files that need to be processed in the "input" folder and access the processed files from the "output" folder
-   -> if multiple files exists and ordering matters then numbers the files by renaming them to preserve th ordering.
+   -> if multiple files exists and ordering matters then numbers the files by renaming them to preserve the ordering.
 
 Choose any one the functionality mentioned below:
     1. Merge PDFs: Combine multiple PDFs into a single document.
@@ -68,9 +76,10 @@ Choose any one the functionality mentioned below:
     4. Extract Images: Save embedded images from PDFs.
     5. Rotate Pages: Rotate individual or all pages in a PDF.
     6. Delete Pages: Remove unwanted pages from a document.
+    7. Watermarking: Add text watermarks to pages.
     
-    7. Next Page
-    8. Exit
+    8. Next Page
+    9. Exit
     
 """
 
@@ -78,8 +87,11 @@ def main() -> None:
     current_menu: int = 1
     ch: int = 0
     output: str
+    values: list[str]
 
+    from os import listdir
     path_list: list[str] = listdir(r".\input")
+    del listdir
 
     if not path_list:
         raise ValueError("No files in the \"output\" folder")
@@ -87,7 +99,7 @@ def main() -> None:
     path_list = [r".\input\\" + path for path in path_list]
 
     while True:
-        eval(f"print('MAIN_MENU_{current_menu}")
+        print(globals()[f"MAIN_MENU_{current_menu}"])
 
         try:
             ch: int = int(input("Enter your choice: "))
@@ -98,49 +110,97 @@ def main() -> None:
         except ValueError:
             input("Enter valid choice. Try Again...")
 
-        match ch:
+        match current_menu:
             case 1:
-                output = input("Enter the output file name: ")
-                merge(path_list, output)
-                input("PDFs has been successfully merged. Check the output folder for the result.")
+                match ch:
+                    case 1:
+                        output = input("Enter the output file name: ")
+                        merge(path_list, output)
+                        input("PDFs has been successfully merged. Check the output folder for the result.")
 
-            case 2:
-                while True:
-                    print("Enter the page no. ranges to split with space separated: ")
-                    print("For Example: 1-3 5-8 13-16")
-                    values: list[str] = input("Enter the values: ").split()
-                    split_range: list[tuple[int, int]] = []
+                    case 2:
+                        while True:
+                            print("Enter the page no. ranges to split with space separated: ")
+                            print("For Example: 1-3 5-8 13-16")
+                            values = input("Enter the values: ").split()
+                            split_range: list[tuple[int, int]] = []
 
-                    try:
-                        for value in values:
-                            first, second = value.split()
-                            split_range.append((int(first), int(second)))
+                            try:
+                                for value in values:
+                                    first, second = value.split('-')
+                                    split_range.append((int(first), int(second)))
 
+                                break
+
+                            except ValueError:
+                                input("Invalid format or input. Try again...")
+
+                        output = input("Enter the output file prefix name: ")
+                        split(path_list[0], tuple(split_range), output)
+                        input("PDF split has been successfully performed. Check the output folder for the results.")
+
+                    case 3:
+                        output = input("Enter the output file name: ")
+                        to_text(path_list[0], output)
+                        input("PDF to Text has been successfully converted. Check the output folder for the result.")
+
+                    case 4:
+                        output = input("Enter the output folder name to store all the images: ")
+                        extract_image(path_list[0], output)
+                        input("Images from the pdf has been successfully extracted. Check the output folder for the result.")
+
+                    case 5:
+                        while True:
+                            values = input("OPTIONAL: Enter the specific page numbers to rotate (space separated): ").split()
+
+                            if values:
+                                try:
+                                    rotate_page: tuple[int] | None = tuple(int(value) for value in values)
+                                    break
+
+                                except ValueError:
+                                    print("Invalid page nos. Try again...")
+
+                            else:
+                                rotate_page = None
+                                break
+
+                        angle: int = int(input("Enter the clockwise angle to rotate (ex-90): "))
+                        output = input("Enter the output file name: ")
+                        rotate(path_list[0], rotate_page, angle, output)
+                        input("The pages of the PDF has successfully rotated. Check the output folder for the result.")
+
+                    case 6:
+                        while True:
+                            values = input("Enter the specific page number to delete (space separated): ").split()
+
+                            try:
+                                delete_page: tuple[int] = tuple(int(value) for value in values)
+                                break
+
+                            except ValueError:
+                                print("Invalid page nos. Try again...")
+
+                        output = input("Enter the output file name: ")
+                        delete(path_list[0], delete_page, output)
+                        input("The pages of the PDF has successfully deleted. Check the output folder for the result.")
+
+                    case 7:
+                        watermark: str = input("Enter the text to have as a watermark: ")
+                        output = input("Enter the output file name: ")
+                        add_watermark(path_list[0], watermark, output)
+                        input("The watermark has been added to the pdf. Check the output folder for the result.")
+
+                    case 8:
+                        current_menu = 2
+
+                    case 9:
+                        print("Thank you for using :)")
                         break
 
-                    except ValueError:
-                        input("Invalid format or input. Try again...")
-
-                output = input("Enter the output file prefix name: ")
-                split(path_list[0], tuple(split_range), output)
-                input("PDF split has been successfully performed. Check the output folder for the results.")
-
-            case 3:
-                output = input("Enter the output file name: ")
-                pdf_to_text(path_list[0], output)
-                input("PDF to Text has been successfully converted. Check the output folder for the result.")
-
-            case 4:
-                output = input("Enter the output folder name to store all the images: ")
-                images_from_pdf(path_list[0], output)
-                input("Images from the pdf has been successfully extracted. Check the output folder for the result.")
-
-            case 7:
-                current_menu = 2
-
-            case 8:
-                print("Thank you for using :)")
-                break
+                    case 0:
+                        clear_directory(r".\output")
+                        input("Output directory was successfully cleared.")
 
 if __name__ == "__main__":
     main()
