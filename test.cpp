babef85c7c8a69d2089fdf2e03d3e4cba5f25404
Dream2503 <swapnarajmohanty2503.com>
2025-03-24 18:38:40 +0530

    commit

--- test.cpp
+++ test.cpp
@@ -1,14 +1,22 @@
-#include "C++/dream"
+#include "C++/dream.h"
 #include <iostream>
 #include <string>
-using namespace Dream;
-
 int main() {
-    string temp("Hello");
+    std::string s;
+    s.replace();
+    s.reserve;
+    s.resize;
+    s.rfind;
+    s.shrink_to_fit;
+    s.size;
+    s.starts_with;
+    s.substr;
+    s.swap;
+    // string temp("Hello");
 
-    for (auto it = temp.begin(); it != temp.end(); it++) {
-        cout << *it;
-    }
+    // for (auto it = temp.rbegin(); it != temp.rend(); it++) {
+    //     cout << *it;
+    // }
     // cout << s;
 
 
