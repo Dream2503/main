6a9723b4a56b627e093bfcb88ef3694e1a2e8e14
Dream2503 <swapnarajmohanty2503.com>
2025-04-22 03:17:31 +0530

    commit

--- python/temst.py
+++ python/temst.py
@@ -10,38 +10,38 @@ FILTERS: tuple[tuple[str]] = (
     ("Ownership", "Corporate"),
     ("Device state", "Managed"),
 )
-HEADER_ORDER = (
+HEADER_ORDER: tuple[str, ...] = (
     "Primary user email address", "Primary user display name", "Manufacturer", "Model", "Enrollment date",
     "Last check-in", "OS version", "Serial number", "IMEI", "MEID", "Subscriber carrier", "Total storage",
     "Free storage", "Ownership", "Device state", "OS", "ICCID", "EID", "Phone number"
 )
-PRIMARY_MAIN = "Primary user email address"
-PRIMARY_LOOKUP = "mail"
-LOOKUP_HEAD = "accountEnabled"
-LOOKUP_VALUE = "True"
+PRIMARY_MAIN: str = "Primary user email address"
+PRIMARY_LOOKUP: str = "mail"
+LOOKUP_HEAD: str = "accountEnabled"
+LOOKUP_VALUE: str = "True"
 
 
-def filter(data: list[list], check_head: str, check_value: str) -> None:
-    head_idx = data[0].index(check_head)
-    delete_idx = [i for i in range(1, len(data)) if data[i][head_idx] != check_value]
+def filter(data: list[list[str]], check_head: str, check_value: str) -> None:
+    head_idx: int = data[0].index(check_head)
+    delete_idx: list[int] = [i for i in range(1, len(data)) if data[i][head_idx] != check_value]
     delete_idx.reverse()
 
     for i in delete_idx:
         del data[i]
 
 
-def replace_blank(data: list[list]) -> None:
+def replace_blank(data: list[list[str]]) -> None:
     for i in range(len(data[0])):
         for j in range(1, len(data)):
             if data[j][i] == "":
                 data[j][i] = BLANK
 
 
-def sort_header(data: list[list]) -> list[list]:
-    res = [[] for _ in range(len(data))]
+def sort_header(data: list[list[str]]) -> list[list][str]:
+    res: list[list[str]] = [[] for _ in range(len(data))]
 
     for value in HEADER_ORDER:
-        head_idx = data[0].index(value)
+        head_idx: int = data[0].index(value)
 
         for j in range(len(data)):
             res[j].append(data[j][head_idx])
@@ -49,12 +49,12 @@ def sort_header(data: list[list]) -> list[list]:
     return res
 
 
-def lookup_remove(main_data: list[list], lookup_data: list[list], primary_main: str, primary_lookup, head: str,
-                  value: str) -> list[list]:
-    primary_main_idx = main_data[0].index(primary_main)
-    primary_lookup_idx = lookup_data[0].index(primary_lookup)
-    head_idx = lookup_data[0].index(head)
-    delete_row_values = []
+def lookup_remove(main_data: list[list[str]], lookup_data: list[list[str]], primary_main: str, primary_lookup,
+                  head: str, value: str) -> list[list[str]]:
+    primary_main_idx: int = main_data[0].index(primary_main)
+    primary_lookup_idx: int = lookup_data[0].index(primary_lookup)
+    head_idx: int = lookup_data[0].index(head)
+    delete_row_values: list[int] = []
 
     for i in range(1, len(main_data)):
         for j in range(1, len(lookup_data)):
@@ -75,19 +75,20 @@ def lookup_remove(main_data: list[list], lookup_data: list[list], primary_main: 
 
 
 def main() -> None:
-    start = perf_counter()
+    start: float = perf_counter()
 
     with open(MAIN_FILE, "r", encoding="utf-8") as f1:
         with open(LOOKUP_FILE, "r", encoding="utf-8") as f2:
-            main_data = list(reader(f1, lineterminator="\n"))
-            lookup_data = list(reader(f2, lineterminator="\n"))
+            main_data: list[list[str]] = list(reader(f1, lineterminator="\n"))
+            lookup_data: list[list[str]] = list(reader(f2, lineterminator="\n"))
 
             for operation in FILTERS:
                 filter(main_data, *operation)
 
             replace_blank(main_data)
-            main_data = sort_header(main_data)
-            main_data = lookup_remove(main_data, lookup_data, PRIMARY_MAIN, PRIMARY_LOOKUP, LOOKUP_HEAD, LOOKUP_VALUE)
+            main_data: list[list[str]] = sort_header(main_data)
+            main_data: list[list[str]] = lookup_remove(main_data, lookup_data, PRIMARY_MAIN, PRIMARY_LOOKUP,
+                                                       LOOKUP_HEAD, LOOKUP_VALUE)
 
             with open(OUTPUT_FILE, "w", encoding="utf-8") as output:
                 output_writer = writer(output, lineterminator="\n")
