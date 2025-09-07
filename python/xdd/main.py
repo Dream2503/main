ad4844d7ae6d38ed059e8dd48ef6e38e219dc33e
Dream2503 <swapnarajmohanty2503.com>
2025-09-07 23:02:53 +0530

    commit

--- python/xdd/main.py
+++
@@ -1,8 +0,0 @@
-from csv import reader
-
-with open("IGFollow__.ko.mal.__300_following_prev.csv", "r", encoding="utf-8") as prev:
-    with open("IGFollow__.ko.mal.__299_following_next.csv", "r", encoding="utf-8") as next:
-        prev = {row[1] for row in reader(prev)}
-        next = {row[1] for row in reader(next)}
-        print(prev - next)
-        print(next - prev)
